7aaee5aba5d546d3e32f2a1e04cf8d4bfde349a7
leilapanova <144453728+leilapanova@users.noreply.github.com>
2024-01-26 14:41:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,113 @@
+
+
+// let arr = ['Привет', ', ', 'мир', '!']
+
+//1
+// console.log(arr.join(''))
+
+
+// 2
+// let text = arr.join('')
+// console.log(text);
+
+
+// 3
+// arr[0] = 'Пока'
+// console.log(arr.join(''))
+
+
+// 4
+// function Obj (employer_name, employer_salary){
+//     this.employer_name = employer_name;
+//     this.employer_salary = employer_salary;
+
+// }
+
+// let user1 = new Obj('Петя', 100000)
+// let user2 = new Obj('Коля', 158000)
+// console.log(user1, user2)
+
+
+// 5
+// let arr = {
+//     'ru': ['голубой', 'красный', 'зеленый'],
+//     'eng': ['blue', 'red', 'green']
+// }
+// console.log(arr['ru'][1])
+
+
+// 6
+// let arr = [1, 2, 3]
+// alert(arr)
+
+
+// 7
+// let arr = [1, 2, 3]
+// for (i of arr){
+
+//     alert(i)
+// }
+
+
+// 8
+// let arr = ['a', 'b', 'c', 'd', 'e']
+// console.log(`${arr[0]}+${arr[1]}, ${arr[2]}+${arr[3]}`);
+
+
+// 9
+// let obj = {a: 1, b: 2, c: 3}
+// console.log(obj.c)
+// console.log(obj[`c`])
+
+
+// 10
+// let obj = {Коля: '1000', Вася: '500', Петя: '200'}
+// console.log(obj['Петя'], obj['Коля'])
+
+
+// 11
+// let days = {
+//     1: 'mn',
+//     2: 'tue',
+//     3: 'wen',
+//     4: 'th',
+//     5: 'fr',
+//     6: 'sat',
+//     7: 'sun'
+// }
+// console.log(days[`5`]);
+
+
+// 12
+// let day = 3
+// console.log(days[day]);
+
+
+// 13
+// let arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+// console.log(arr[1][0])
+
+
+// 14
+// let obj = { js: ['JQuery', 'Angular'],
+//             css : 'world',
+//             php : 'hello'
+// };
+// console.log(obj.js[0])
+
+
+// 15
+// let arr = {
+//     'ru': ['понедельник', 'вторник', "среда", "четверг", "пятница", "суббота", "воскресенье"],
+//     'eng': ['mon', 'tu', 'wen', 'th', 'fr', 'sat', 'sun']
+// }
+// console.log(arr.ru[0], arr.eng[2]);
+
+
+
+
+
